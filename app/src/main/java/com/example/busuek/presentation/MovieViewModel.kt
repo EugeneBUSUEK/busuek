55308b24bb1e837f747873a2a2d96e7b6b0f503b
EugeneBUSUEK <Eugeny.BUSUEK@gmail.com>
2023-02-05 18:17:14 +0300

    end working on view model for second screen

--- app/src/main/java/com/example/busuek/presentation/MovieViewModel.kt
+++ app/src/main/java/com/example/busuek/presentation/MovieViewModel.kt
@@ -1,5 +1,7 @@
 package com.example.busuek.presentation
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.busuek.data.MoviesRepositoryImpl
 import com.example.busuek.domain.AddMovieUseCase
@@ -7,15 +9,32 @@ import com.example.busuek.domain.EditMovieUseCase
 import com.example.busuek.domain.GetMovieUseCase
 import com.example.busuek.domain.Movie
 
-class MovieViewModel: ViewModel() {
+class MovieViewModel : ViewModel() {
 
     private val repository = MoviesRepositoryImpl
     private val getMovieUseCase = GetMovieUseCase(repository)
     private val addMovieUseCase = AddMovieUseCase(repository)
     private val editMovieUseCase = EditMovieUseCase(repository)
 
+    private val _errorInputName = MutableLiveData<Boolean>()
+    val errorInputName: LiveData<Boolean>
+        get() = _errorInputName
+
+    private val _errorInputCount = MutableLiveData<Boolean>()
+    val errorInputCount: LiveData<Boolean>
+        get() = _errorInputCount
+
+    private val _movie = MutableLiveData<Movie>()
+    val movie: LiveData<Movie>
+        get() = _movie
+
+    private val _shouldCloseScreen = MutableLiveData<Unit>()
+    val shouldCloseScreen: LiveData<Unit>
+        get() = _shouldCloseScreen
+
     fun getMovie(movieId: Int) {
         val movie = getMovieUseCase.getMovie(movieId)
+        _movie.value = movie
     }
 
     fun addMovie(inputName: String?, inputCount: String?) {
@@ -25,6 +44,7 @@ class MovieViewModel: ViewModel() {
         if (fieldsValid) {
             val movie = Movie(name, "", "", "", "", "", false, count)
             addMovieUseCase.addMovie(movie)
+            finishWork()
         }
     }
 
@@ -33,8 +53,11 @@ class MovieViewModel: ViewModel() {
         val count = parseCount(inputCount)
         val fieldsValid = validateInput(name, count)
         if (fieldsValid) {
-            val movie = Movie(name, "", "", "", "", "", false, count)
-            editMovieUseCase.editMovie(movie)
+            _movie.value?.let {
+                val movie = it.copy(name = name, id = count)
+                editMovieUseCase.editMovie(movie)
+                finishWork()
+            }
         }
     }
 
@@ -53,13 +76,25 @@ class MovieViewModel: ViewModel() {
     private fun validateInput(name: String, count: Int): Boolean {
         var result = true
         if (name.isBlank()) {
-            //TODO(): show error input name
+            _errorInputName.value = true
             result = false
         }
         if (count <= 0) {
-            //TODO(): show error input count
+            _errorInputCount.value = true
             result = false
         }
         return result
+    }
+
+    public fun resetErrorInputName() {
+        _errorInputName.value = false
+    }
+
+    public fun resetErrorInputCount() {
+        _errorInputCount.value = false
+    }
+
+    private fun finishWork() {
+        _shouldCloseScreen.value = Unit
     }
 }
